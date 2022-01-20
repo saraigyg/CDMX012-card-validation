7e1f7041004943ae12ddde2127ebd5117ae5ab6a
sarai gonzalez <saraiglezyglez@gmail.com>
2022-01-19 20:16:37 -0600

    agregar funcionalidada numero de tarjeta

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 import validator from './validator.js';
-
 console.log(validator);
+
+
+
+  /***********************input card number ***********/
+  /*******numbers only**************/
+  /*function numbersOnly(event) {
+      return event.charCode === 0 || /\d/.test(String.fromCharCode(event.charCode));
+  }
+
+  const input = document.querySelector("input");
+  input.onkeydown = numbersOnly;
+
+  input.onpaste = Event => false;
+*/
+
+
+/**** function button next******/
+function next () {
+  document.getElementById("secondWindow").style.display = "none";
+}
+
+function next() {
+  let x = document.getElementById("secondWindow");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+/***function button reload**** */
+const reload = document.getElementById("reset");
+reload.addEventListener("click",_=> {location.reload();})
+
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,16 @@
+/****function button validate */
 const validator = {
-  // ...
-};
+  function algorithm() {
+  let array = document.getElementById("user_cardNumber");
+  } console.log(array);
+}
+//const validator = {
+ // ...
+
+/***Sorry, it looks like it is an invalid card number please reset here*/
+
+/***Nice easily done! The shipping process starts now. Thanks for buying with us! */
+
+
 
 export default validator;
