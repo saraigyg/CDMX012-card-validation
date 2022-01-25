62b3228b923b1242c850d3084b2fdf2c88f76edb
sarai gonzalez <saraiglezyglez@gmail.com>
2022-01-24 21:11:17 -0600

    segundo y útlimo commit

--- src/index.js
+++ src/index.js
@@ -1,39 +1,18 @@
 import validator from './validator.js';
 console.log(validator);
 
+console.log(validator.myFunction(""));
+console.log(validator.myFunctionTwo(""));
 
 
-  /***********************input card number ***********/
-  /*******numbers only**************/
-  /*function numbersOnly(event) {
-      return event.charCode === 0 || /\d/.test(String.fromCharCode(event.charCode));
-  }
 
-  const input = document.querySelector("input");
-  input.onkeydown = numbersOnly;
-
-  input.onpaste = Event => false;
-*/
-
-
-/**** function button next******/
-function next () {
-  document.getElementById("secondWindow").style.display = "none";
-}
-
-function next() {
-  let x = document.getElementById("secondWindow");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-
-/***function button reload**** */
-const reload = document.getElementById("reset");
-reload.addEventListener("click",_=> {location.reload();})
-
-
+//funcion que al dar click en el boton validate corra mi función para validar ocupando el valor del usuario //
+let buttonVal = document.getElementById("buttonValidate"); 
+buttonVal.addEventListener("click", function() {
+  let validate = validator.myFunction(document.getElementById("user_cardNumber").value);
+  (document.getElementById("isValid").innerHTML)=validate;
+  let maskify = validator.myFunctionTwo(document.getElementById("user_cardNumber").value); 
+  (document.getElementById("kitty").innerHTML)=maskify;
+});
 
 
---
+++ src/javascript.js
@@ -0,0 +1,13 @@
+const estuche = {
+    color: "rojo",
+    medidas: {
+        longitud:25,
+        altura:20,
+        ancho:15
+    },
+    tamaño:"mediano",
+    dibujos:true,
+    relleno:false,
+}
+
+console.log(estuche.medidas);
