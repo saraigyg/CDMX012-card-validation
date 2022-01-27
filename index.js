c881ff73f86ee7ea8ce773915f2191db9ad681ad
sarai gonzalez <saraiglezyglez@gmail.com>
2022-01-26 18:12:21 -0600

    Updates

---
+++ index.js
@@ -0,0 +1,18 @@
+import validator from './validator.js';
+//console.log(validator);
+
+//console.log(validator.myFunction(""));
+//console.log(validator.myFunctionTwo(""));
+
+
+
+//funcion que al dar click en el boton validate corra mi función para validar ocupando el valor del usuario //
+let buttonVal = document.getElementById("buttonValidate"); 
+buttonVal.addEventListener("click", function() {
+  let validate = validator.isValid(document.getElementById("user_cardNumber").value);
+  (document.getElementById("isValid").innerHTML)=validate;
+  let maskify = validator.maskify(document.getElementById("user_cardNumber").value); 
+  (document.getElementById("kitty").innerHTML)=maskify;
+});
+
+
---
+++ validator.js
@@ -0,0 +1,48 @@
+/****function button validate *****/
+const validator = {
+ isValid: function isValid (array) {
+   //pasar de un objeto tipo string a un arreglo separado por comas y guardarlo en una variable n 
+   if (array == 0) {
+     return "Please, insert a card number";
+   }
+   let n = array.split("");
+   //pasar el arreglo de string separado por comas a un nuevo arreglo pero numérico y en orden inverso, eso guardarlo en
+   //una variable x
+   let x = n.map((i) => Number(i)).reverse();
+   //saber la longitud de ese array para poder encontrar las posiciones impares
+   for (let i=0; i < x.length; i++) {
+     if (i % 2 !== 0) {
+  //multiplicar el numero en posición impar por dos y guardar la variable
+      x[i]=x[i]*2;
+    // si ese numero multiplicado es mayor o igual a 10
+     } if (x[i] >= 10) {
+    //pasar el número a un arreglo tipo string y separar ese arreglo mediante comas y 
+    //después pasarlo a un nuevo arreglo numérico compuesto cada par de dos numeros impares 
+    //y guardar en variable s
+      let s = x[i].toString().split("").map((i) => Number(i));
+    //ahora x[i] será igual a la suma del valor de la posición 0 más la suma del valor de la posición 1   
+      x[i] = s[0] + s[1]; 
+     } 
+     //suma total o reducción a un solo valor de todo el arreglo x incluyendo los nuevos números del paso anterior y guardo en una variable 
+   } let total = x.reduce((a,b) => a + b, 0);
+   //si el valor de la suma total es múltiplo de 10
+    if (total % 10 === 0) {
+  //imprime valid card
+    return true; 
+  //console.log("Valid Card Number");
+   } 
+  //imprime invalid card al no ser múltiplo de 10 
+      else {
+     return false;
+        //console.log("Invalid Card Number");
+   }
+  },
+  //para insertar el # en todo el arreglo menos en los últimos cuatro dígitos 
+  maskify: function maskify (mask) {
+  //el objeto tipo arreglo insertado cambiarlo a string [" "] y éste será remplazado con aserción anticipada que reemplace los últimos 4 con #
+    let maskify = mask.toString().replace(/.(?=.{4})/g, "#"); 
+    return (maskify);
+    //console.log(maskify); 
+  }
+}
+export default validator;
